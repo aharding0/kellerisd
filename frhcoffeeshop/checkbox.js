81cc365ebb36ae1809a6aa850402f059b95b6c35
aharding0 <78751695+aharding0@users.noreply.github.com>
2021-02-25 14:04:52 -0600

    asdf

--- frhcoffeeshop/checkbox.js
+++ frhcoffeeshop/checkbox.js
@@ -1,6 +1,16 @@
-$('input[type=checkbox]').on('change', function (e) {
-    if ($('input[type=checkbox]:checked').length > 3) {
-        $(this).prop('checked', false);
-        alert("allowed only 3");
-    }
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.4.4/jquery.js"></script>
+$(document).ready(function() {
+
+    $("#type").change(function() {
+        var val = $(this).val();
+        $("#size").html(options[val]);
+    });
+
+
+    var options = [
+        "<option value='test'>item1: test 1</option><option value='test2'>item1: test 2</option>",
+        "<option value='test'>item2: test 1</option><option value='test2'>item2: test 2</option>",
+        "<option value='test'>item3: test 1</option><option value='test2'>item3: test 2</option>"
+    ];
+
 });
